cc46472996fe524ddb7e29c3144f11b24b1b9e7e
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2020-12-23 17:07:41 -0300

    21-funciones-anonimas

--- public_html/js/21-funciones_anonimas.js
+++ public_html/js/21-funciones_anonimas.js
@@ -1,15 +1,21 @@
 'use stric';
-var pelicula= function (nombre){
-    return 'La pelicula es: '+nombre;
+//funciones anonimas y funcion de flecha
+
+let pelicula=function(nombre){
+    return `La pelicula es ${nombre}`;
 }
+
+//console.log(pelicula("Spiderman"))
+
 function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
-    var sumar= numero1 + numero2;
-    sumaYmuestra(sumar);
-    sumaPorDos(sumar);
-    return sumar;
+    const suma= numero1+numero2;
+    sumaYmuestra(suma);
+    sumaPorDos(suma);
+    return suma;
 }
-sumame(5,7,function(dato){
-    console.log("La suma es: "+dato);
-},function(dato){
-    console.log("La suma por dos es: "+(dato*2));
-});
+
+sumame(5,1, dato=>{
+    console.log(`La suma es: ${dato}`);
+    }, dato=>{
+        console.log(`La suma por dos es: ${dato*2}`)
+        })
