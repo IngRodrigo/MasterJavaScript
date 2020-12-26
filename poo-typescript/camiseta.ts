e9dbf1299081f1d2c2dfa85ba06193d595989258
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2020-12-26 10:33:15 -0300

    TypeSript

--- poo-typescript/camiseta.ts
+++ poo-typescript/camiseta.ts
@@ -1,9 +1,53 @@
-//clase
-class Camiseta{
-    public color:string;
-    public modelo:string;
-    public marca:string;
-    public talla:string;
-    public precio:number;
+//Decoradores : Agrega una funcion a la clase en la que lo implementemos
+//se implementa de la siguente forma @midecorador
+
+function midecorador(logo:string){
+    return function(target:Function){
+        target.prototype.estampacion=function():void{
+            console.log(`Camiseta estampada con el logo: ${logo}`)
+        }
+    }
+
 }
-//metodos
+
+//Interface
+interface CamisetaBase{
+    getColor();
+    setColor(color:string);
+}
+
+//clase
+@midecorador('rod-dev')
+class Camiseta implements CamisetaBase{
+    //Propiedades
+    private color:string;
+    private modelo:string;
+    private marca:string;
+    private talla:string;
+    private precio:number;
+    //Metodos
+    public setColor(color:string){
+        this.color=color;
+    }
+    public getColor():string{
+        return this.color;
+    }
+    //constructor
+    constructor(color, modelo, marca, precio){
+        this.color=color;
+        this.modelo=modelo;
+        this.marca=marca;
+        this.precio=precio;
+    }
+}
+
+//clase hija
+
+class sudadera extends Camiseta{
+
+}
+
+let sudaderaNew= new sudadera("Verde","mangas cortas", "nike", 500);
+
+console.log(sudaderaNew);
+
