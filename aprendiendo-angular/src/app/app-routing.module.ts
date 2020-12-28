787a5cec7ac5c87cedf9235ee7bb556e01f8fe19
IngRodrigo <romero10rodrigo@gmail.com>
2020-12-28 18:49:42 -0300

    Implementando rutas

--- aprendiendo-angular/src/app/app-routing.module.ts
+++ aprendiendo-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component:HomeComponent},
+{path: 'home', component:HomeComponent},
+{path: 'zapatillas', component:ZapatillasComponent},
+{path:'cursos', component:CursosComponent},
+{path:'**', component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -13,13 +13,15 @@ import { AppComponent } from './app.component';
 import {VideojuegoComponent} from './videoJuego/videojuego.componet';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 //los componentes, pipes, van siempre en declarations
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     ZapatillasComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
