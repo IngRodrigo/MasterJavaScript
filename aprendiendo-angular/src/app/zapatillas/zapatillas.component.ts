f12cb0d44d6edcb7c540506ec3eb60a232aa49cf
IngRodrigo <romero10rodrigo@gmail.com>
2020-12-28 18:05:52 -0300

    Directivas

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -1,5 +1,5 @@
 import { from } from "rxjs";
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Zapatilla} from '../models/zapatilla';
 
 @Component(
@@ -9,17 +9,51 @@ import {Zapatilla} from '../models/zapatilla';
     }
 )
 
-export class ZapatillasComponent{
+export class ZapatillasComponent implements OnInit{
     public titulo:string;
+    public mi_marca:string;
+    
     public zapatillas=new Array(
         new Zapatilla('Nike 10', 'Nike', 'Yellow', 1200, false),
         new Zapatilla('Nike 10', 'Nike', 'Black', 2000, true),
         new Zapatilla('Nike 20', 'Nike', 'Red', 1000, false),
         new Zapatilla('Adidas RE', 'Adidas', 'Blue', 1300, true)    
     );
+
+    public marcas: String[];
     constructor(){
+        this.mi_marca="File";
+        this.marcas=new Array();
         this.titulo=`Componente de zapatillas`;
         console.log(`Componete zapatilla cargado correctamente`);
         //console.log(this.zapatilla);
+    }
+    ngOnInit(){
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach(zapatilla => {
+
+            //buscara la marca dentro del array marcas con el indexOf devuelve -1 cuando no encuentra el elemento
+            if(this.marcas.indexOf(zapatilla.marca)<0){
+
+                this.marcas.push(zapatilla.marca);
+            }
+
+        });
+
+
+        //console.log(this.marcas);
+    }
+    addMarca(){
+            this.marcas.push(this.mi_marca);
+    }
+    borrarMarca(indice:any){
+        this.marcas.splice(indice, 1);
+    }
+
+    mostrarMarca(){
+        alert(this.mi_marca);
     }
 }
